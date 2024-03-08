b7024a279ebca9f92fe2c6b2884162e59ed780f0
kuskyst <pe6_7@yahoo.co.jp>
2024-03-08 19:35:15 +0900

    add dice

--- mtg-life-counter-ios/ui/Buttons.swift
+++ mtg-life-counter-ios/ui/Buttons.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct Buttons: View {
 
     @Binding var life: LifeEntity
+    @State private var dice = 1
+    @State private var visible = false
 
     var body: some View {
         VStack {
@@ -26,8 +28,18 @@ struct Buttons: View {
             .cornerRadius(8.0)
             .padding(12.0)
             Spacer()
+            if visible {
+                Image("dice\(dice)")
+                    .renderingMode(.template)
+                    .resizable()
+                    .foregroundColor(Color.white)
+                    .scaledToFit()
+                    .frame(width: 160.0, height: 160.0)
+            }
+            Spacer()
             Button(action: {
-            
+                self.visible = !self.visible
+                self.dice = Int.random(in: 1..<7)
             }) {
                 Text("dice")
                     .padding(8.0)
@@ -42,5 +54,6 @@ struct Buttons: View {
 
 #Preview {
     @State var previewLife = LifeEntity(date: "1", left: 20, right: 20)
+    @State var previewDice = 1
     return Buttons(life: $previewLife)
 }
