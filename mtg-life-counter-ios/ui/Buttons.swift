fb49aa5b22dbb16fd0cf63b74b4066f6eaa89353
kuskyst <pe6_7@yahoo.co.jp>
2024-03-08 20:03:50 +0900

    add dice animation

--- mtg-life-counter-ios/ui/Buttons.swift
+++ mtg-life-counter-ios/ui/Buttons.swift
@@ -28,18 +28,20 @@ struct Buttons: View {
             .cornerRadius(8.0)
             .padding(12.0)
             Spacer()
-            if visible {
-                Image("dice\(roll)")
-                    .renderingMode(.template)
-                    .resizable()
-                    .foregroundColor(Color.white)
-                    .scaledToFit()
-                    .frame(width: 160.0, height: 160.0)
-            }
+            Image("dice\(roll)")
+                .renderingMode(.template)
+                .resizable()
+                .foregroundColor(Color.white)
+                .scaledToFit()
+                .frame(width: 160.0, height: 160.0)
+                .scaleEffect(visible ? 0.8 : 0)
+                .rotationEffect(Angle(degrees: visible ? 360 : 0))
+                .offset(x: visible ? 0 : -50, y: visible ? 0 : -50)
+                .animation(.easeInOut(duration: 0.7), value: visible)
             Spacer()
             Button(action: {
-                self.visible = !self.visible
-                self.roll = Int.random(in: 1..<7)
+                withAnimation { self.visible = !self.visible }
+                if self.visible { self.roll = Int.random(in: 1..<7) }
             }) {
                 Text("dice")
                     .padding(8.0)
